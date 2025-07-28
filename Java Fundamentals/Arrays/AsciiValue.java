434f7a6977ccbda84010f17255acf548f6950672
Trishla09 <trishladeepu1209@gmail.com>
2025-07-28 20:05:18 +0530

    Add files via upload

---
+++ Java Fundamentals/Arrays/AsciiValue.java
@@ -0,0 +1,15 @@
+package Arrayss;
+
+public class AsciiValue {
+
+	public static void main(String[] args) {
+		int []arr= {84,114,73,115,72,108,65};
+		for(int i:arr) 
+			System.out.print((char)(i)+" ");
+		
+		
+		
+		
+	}
+
+}
